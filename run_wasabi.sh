1dc8b7beac6c0b7133a423f3211cdd637dbe239b
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-19 15:04:05 +0100

    feat: add nginx ssl config example

--- run_wasabi.sh
+++ run_wasabi.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 mkdir -p data
 docker run --restart unless-stopped -p 8095:8095 -d -v $(pwd)/data:/app/bin/data hellonico/wasabi:latest
+
+# To enable SSL (HTTPS) for wasabi.hellonico.info:
+# 1. Copy nginx.conf.example to /etc/nginx/sites-available/wasabi.hellonico.info
+# 2. ln -s /etc/nginx/sites-available/wasabi.hellonico.info /etc/nginx/sites-enabled/
+# 3. nginx -t && systemctl reload nginx
+# 4. sudo certbot --nginx -d wasabi.hellonico.info
+#    (This will generate a new cert OR expand existing cert if you choose)
+
